af13847cb4cb6b12804e0c55574bb21ca2a47ebd
ramaarief <ramaarief45@gmail.com>
2020-07-23 23:53:18 +0700

    Eloquent

--- app/Artikel.php
+++ app/Artikel.php
@@ -3,9 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Kategori;
 
 class Artikel extends Model
 {
     protected $table = 'artikel';
     protected $fillable = ['judul', 'cuplikan', 'isi', 'gambar', 'waktu_artikel'];
+
+    public function kategori()
+    {
+        return $this->hasMany(Kategori::class);
+    }
 }
--- app/Kategori.php
+++ app/Kategori.php
@@ -3,9 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Artikel;
 
 class Kategori extends Model
 {
     protected $table = 'kategori';
     protected $fillable = ['artikel_id', 'nama_kategori'];
+
+    public function artikel()
+    {
+        return $this->belongsTo(Artikel::class);
+    }
 }
